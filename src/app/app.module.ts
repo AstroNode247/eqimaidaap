e671a670b42cb865d334ff928283c410733d306f
andry.anthony <astro@node.com>
2023-02-10 13:36:16 +0300

    UI design for the sign in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FingerprintComponent } from './components/fingerprint/fingerprint.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    SigninComponent,
+    HomeComponent,
+    FingerprintComponent
   ],
   imports: [
     BrowserModule,
